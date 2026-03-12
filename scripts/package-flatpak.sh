9d47181baa7e1e1a5188ba6bc2ec06372dc22e5b
q962 <arcere@qq.com>
2026-03-12 15:53:07 +0800

    chore: update flatpak

--- scripts/package-flatpak.sh
+++ scripts/package-flatpak.sh
@@ -7,6 +7,6 @@ cd `dirname $0`
 git ls-files --recurse-submodules .. | tar caf archive.tar -T-
 
 flatpak-builder --state-dir=../build/.flatpak-builder ../build/flatpak-build-dir flatpak.yml  --force-clean --user --install --disable-updates --repo=../build/.flatpak-repo "$@"
-flatpak install -u -y --reinstall `flatpak info -o io.github.q962.PictureWindow` io.github.q962.PictureWindow.Locale//test
+flatpak install -u -y --reinstall `flatpak info -o io.github.q962.PictureWindow//test` io.github.q962.PictureWindow.Locale//test
 
 flatpak build-bundle ../build/.flatpak-repo io.github.q962.PictureWindow.flatpak  io.github.q962.PictureWindow test
